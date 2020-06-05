d7ac8ecf30d92b0aced23d1a74726343f24e23d4
Angela Bumgarner <amrvitale@gmail.com>
2020-06-04 23:10:45 -0700

    everything working locally except for date

---
+++ src/Note/Note.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+//import  format  from 'date-fns/format'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ApiContext from '../ApiContext'
+import config from '../config'
+import './Note.css'
+
+export default class Note extends React.Component {
+  static defaultProps ={
+    onDeleteNote: () => {},
+  }
+  static contextType = ApiContext;
+
+  handleClickDelete = e => {
+    e.preventDefault()
+    const noteId = this.props.id
+
+    fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json'
+      },
+    })
+      .then(res => {
+        if (!res.ok)
+          return res.json().then(e => Promise.reject(e))
+        return res.json()
+      })
+      .then(() => {
+        this.context.deleteNote(noteId)
+        // allow parent to perform extra behaviour
+        this.props.onDeleteNote(noteId)
+      })
+      .catch(error => {
+        console.error({ error })
+      })
+  }
+
+  render() {
+    const { name, id, date_modified } = this.props 
+    const date = new Date(date_modified);
+
+    return (
+      <div className='Note'>
+        <h2 className='Note__title'>
+          <Link to={`/note/${id}`}>
+            {name}
+          </Link>
+        </h2>
+        <button
+          className='Note__delete'
+          type='button'
+          onClick={this.handleClickDelete}
+        >
+          <FontAwesomeIcon icon='trash-alt' />
+          {' '}
+          remove
+        </button>
+        <div className='Note__dates'>
+          <div className='Note__dates-modified'>
+            <p>Modified {date.toDateString()}</p>   
+            <span className='Date'>
+            </span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+//            {format('yyyy-LL-dd', new Date())}

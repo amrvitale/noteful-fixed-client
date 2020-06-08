8d2f7ac24755468b92fbcb9fa9ef1f9c8e09f4e2
Angela Bumgarner <amrvitale@gmail.com>
2020-06-07 20:31:03 -0700

    updated url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
 export default {
-    API_ENDPOINT: 'https://evening-dawn-21463.herokuapp.com/api',
+    API_ENDPOINT: `https://afternoon-shelf-20887.herokuapp.com/api`,
+
   }
+//
+//http://localhost:9090

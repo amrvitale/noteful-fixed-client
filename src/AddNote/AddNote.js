e01500a50d04a0abe69f1de84660523671ea4c60
Angela Bumgarner <amrvitale@gmail.com>
2020-06-09 14:26:04 -0700

    updated folderId to folder_id across all applicable files

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -12,14 +12,27 @@ export default class AddNote extends Component {
   }
   static contextType = ApiContext;
 
+ constructor(props) {
+   super(props);
+   this.state = {value: ''};
+
+   this.handleChange = this.handleChange.bind(this);
+   this.handleSubmit = this.handleSubmit.bind(this);
+ }
+
+ handleChange = e => {
+  this.setState({[e.target.getAttribute('name')]: e.target.value});
+  }
+
   handleSubmit = e => {
     e.preventDefault()
     const newNote = {
-      name: e.target['note-name'].value,
-      content: e.target['note-content'].value,
-      folderId: e.target['note-folder-id'].value,
+      name: this.state.name,
+      content: this.state.content,
+      folder_id: this.state.folder_id,
       modified: new Date(),
     }
+    console.log(newNote)
     fetch(`${config.API_ENDPOINT}/notes`, {
       method: 'POST',
       headers: {
@@ -36,12 +49,13 @@ export default class AddNote extends Component {
       })
       .then(note => {
         this.context.addNote(note)
-        this.props.history.push(`/folder/${note.folderId}`)
+        this.props.history.push(`/folder/${note.folder_id}`)
       })
       .catch(error => {
         console.error({ error })
       })
   }
+ 
 
   render() {
     const { folders=[] } = this.context
@@ -53,19 +67,19 @@ export default class AddNote extends Component {
             <label htmlFor='note-name-input'>
               Name
             </label>
-            <input type='text' id='note-name-input' name='note-name' />
+            <input type='text' id='note-name-input' name='name' onChange={this.handleChange} />
           </div>
           <div className='field'>
             <label htmlFor='note-content-input'>
               Content
             </label>
-            <textarea id='note-content-input' name='note-content' />
+            <textarea id='note-content-input' name='content' onChange={this.handleChange} />
           </div>
           <div className='field'>
             <label htmlFor='note-folder-select'>
               Folder
             </label>
-            <select id='note-folder-select' name='note-folder-id'>
+            <select id='note-folder-select' name='folder_id' onChange={this.handleChange}>
               <option value={null}>...</option>
               {folders.map(folder =>
                 <option key={folder.id} value={folder.id}>

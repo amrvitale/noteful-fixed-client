7e873a14467ae406517da8fc0408548acdf3f512
Angela Bumgarner <amrvitale@gmail.com>
2020-06-09 14:49:09 -0700

    updated notes-helpers.js to allow notes visible in folders

--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -7,7 +7,7 @@ export const findNote = (notes=[], noteId) =>
 export const getNotesForFolder = (notes=[], folder_id) => (
   (!folder_id)
     ? notes
-    : notes.filter(note => note.folder_id === folder_id)
+    : notes.filter(note => note.folder_id.toString() === folder_id.toString())
 )
 
 export const countNotesForFolder = (notes=[], folder_id) =>

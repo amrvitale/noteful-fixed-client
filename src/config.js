8d2120c55eca3c9f1d7f23fa4f00e681db765310
Angela Bumgarner <amrvitale@gmail.com>
2020-06-11 11:32:36 -0700

    changing to production urls

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 export default {
-    API_ENDPOINT: `http://localhost:8000/api`,
-
+    //API_ENDPOINT: `http://localhost:8000/api`,
+    API_ENDPOINT: `https://angelas-noteful-server.herokuapp.com/api`
   }
 //
 //
 
-//https://arcane-castle-16152.herokuapp.com/api
